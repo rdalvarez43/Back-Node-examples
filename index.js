de58042aabbf71a22ab4e31acd0ddc6190206090
rdalvarez43 <rdalvarez43@gmail.com>
2020-10-26 11:57:39 +0100

    MathJS example

---
+++ index.js
@@ -0,0 +1,13 @@
+
+const { sqrt } = require('mathjs')
+
+
+
+console.log("Hola Mundo ahora en back end")
+
+let numero1 = 169;
+//Math.sqrt()
+
+
+console.log(`La raiz cuadrada ${numero1}  es ${sqrt(numero1).toString()}`);
+// Esto es lo mismo de lo de arriba console.log("La raiz cuadrada" + numero1 )
